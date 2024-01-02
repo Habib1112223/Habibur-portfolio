9eb57c47a679cb1b2820dbb0c02c93ef95560175
Habibur1112223 <webdevelopment.habib2023@gmail.com>
2024-01-03 01:03:10 +0600

    create the about section for the portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/no-unescaped-entities */
+const About = () => {
+      return (
+        <div
+          name="about"
+          className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+        >
+          <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+            <div className="pb-8">
+              <p className="text-4xl mt-12 font-bold inline border-b-4 border-gray-500">
+                About
+              </p>
+            </div>
+            <p className="text-xl md:mt-20">
+            Hello, I'm Md. Habibur Rahman, a dedicated Front End Developer based in Dhaka, Bangladesh. I take great pride in crafting engaging and user-friendly interfaces, bringing creativity and functionality to the forefront of web development.I thoroughly enjoy working with the MERN stack, finding the seamless integration of MongoDB, Express.js, React, and Node.js not only exhilarating but also highly efficient for building robust and dynamic web applications.I take pleasure in designing websites that not only boast visually appealing aesthetics but also function seamlessly. Continuously staying abreast of the latest advancements in web development, I strive to leverage the best tools available. Eager to contribute my skills to compelling projects and deliver exceptional, user-friendly websites.I have successfully completed my graduation in Computer Science and Engineering from the National University. This educational journey has equipped me with a strong foundation in the field, and I am now eager to apply my knowledge and skills in practical settings.
+            </p>
+            <br />
+          </div>
+        </div>
+      );
+    };
+    
+    export default About;
